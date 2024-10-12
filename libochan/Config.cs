3460a6d0603fd95c97b9b4f61151127a9f959c05
Tom F. Grassmann <tom.grassmann@coding-pioneers.com>
2024-10-12 21:40:32 +0200

    namespace and types

--- libochan/Config.cs
+++ libochan/Config.cs
@@ -1,63 +1,62 @@
+namespace oChan;
 using System;
 using System.IO;
 using Serilog;
 
-namespace oChan
+public class Config
 {
-    public class Config
+    public string DownloadPath { get; set; }
+
+    // Constructor with a default download path
+    public Config()
     {
-        public string DownloadPath { get; set; }
+        Log.Debug("Initializing Config instance.");
 
-        // Constructor with a default download path
-        public Config()
-        {
-            Log.Debug("Initializing Config instance.");
-
-            // Initially hardcoded, but later read from config file
-            DownloadPath = GetDefaultPath();
-        }
-
-        private string GetDefaultPath()
-        {
-            try
-            {
-                string path;
-                // Get a default path based on the OS
-                if (OperatingSystem.IsWindows())
-                {
-                    path = Path.Combine(
-                        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                        "oChanDownloads");
-                    Log.Debug("Default download path for Windows: {DownloadPath}", path);
-                }
-                else if (OperatingSystem.IsLinux() || OperatingSystem.IsMacOS())
-                {
-                    path = Path.Combine(
-                        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                        "oChanDownloads");
-                    Log.Debug("Default download path for Unix-based OS: {DownloadPath}", path);
-                }
-                else
-                {
-                    Log.Error("Unsupported operating system detected.");
-                    throw new PlatformNotSupportedException("Unsupported OS");
-                }
-                return path;
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "An error occurred while getting the default download path.");
-                throw;
-            }
-        }
-
-        // Method to print configuration settings (for testing)
-        public void PrintConfig()
-        {
-            Log.Information("Configuration settings:");
-            Log.Information("Download Path: {DownloadPath}", DownloadPath);
-        }
-
-        // Later, add methods to save/load from a config file
+        // Initially hardcoded, but later read from config file
+        DownloadPath = GetDefaultPath();
     }
+
+    private string GetDefaultPath()
+    {
+        try
+        {
+            string path;
+            // Get a default path based on the OS
+            if (OperatingSystem.IsWindows())
+            {
+                path = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                    "oChanDownloads");
+                Log.Debug("Default download path for Windows: {DownloadPath}", path);
+            }
+            else if (OperatingSystem.IsLinux() || OperatingSystem.IsMacOS())
+            {
+                path = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                    "oChanDownloads");
+                Log.Debug("Default download path for Unix-based OS: {DownloadPath}", path);
+            }
+            else
+            {
+                Log.Error("Unsupported operating system detected.");
+                throw new PlatformNotSupportedException("Unsupported OS");
+            }
+            return path;
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occurred while getting the default download path.");
+            throw;
+        }
+    }
+
+    // Method to print configuration settings (for testing)
+    public void PrintConfig()
+    {
+        Log.Information("Configuration settings:");
+        Log.Information("Download Path: {DownloadPath}", DownloadPath);
+    }
+
+    // Later, add methods to save/load from a config file
 }
+
--- libochan/Utils.cs
+++ libochan/Utils.cs
@@ -1,35 +1,35 @@
+namespace oChan;
+
 using System;
 using System.IO;
 
-namespace oChan
+public static class Utils
 {
-    public static class Utils
+    public static string ToHumanReadableSize(long bytes)
     {
-        public static string ToHumanReadableSize(long bytes)
+        string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+        double len = bytes;
+        int order = 0;
+        while (len >= 1024 && order < sizes.Length - 1)
         {
-            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
-            double len = bytes;
-            int order = 0;
-            while (len >= 1024 && order < sizes.Length - 1)
-            {
-                order++;
-                len /= 1024;
-            }
-            return $"{len:0.##} {sizes[order]}";
+            order++;
+            len /= 1024;
         }
-    }
-        public static class PathSanitizer
-    {
-        public static string SanitizePath(string path)
-        {
-            // Remove invalid characters
-            var invalidChars = Path.GetInvalidPathChars();
-            foreach (var c in invalidChars)
-            {
-                path = path.Replace(c.ToString(), "");
-            }
-
-            return path;
-        }
+        return $"{len:0.##} {sizes[order]}";
     }
 }
+public static class PathSanitizer
+{
+    public static string SanitizePath(string path)
+    {
+        // Remove invalid characters
+        char[] invalidChars = Path.GetInvalidPathChars();
+        foreach (char c in invalidChars)
+        {
+            path = path.Replace(c.ToString(), "");
+        }
+
+        return path;
+    }
+}
+
